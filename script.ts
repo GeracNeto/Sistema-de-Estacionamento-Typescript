68dcd44eebed43b97b78451d2ea22dc517c134cd
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-10 20:23:25 -0300

    Criada as funções para adicionar e salvar os dados no LocalStorage

--- script.ts
+++ script.ts
@@ -1,8 +1,56 @@
-// Função anônima
+// Interface
 
+interface Veiculo{
+    nome: String;
+    placa: String;
+    entrada: Date;
+}
+
+
+// Função anônima
 (function(){
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
 
+     function patio(){
+        function ler(): Veiculo[]{
+            return localStorage.patio ? JSON.parse(localStorage.patio): [];
+        };
+
+        function salvar(veiculos: Veiculo[]){
+            localStorage.setItem('patio', JSON.stringify(veiculos))
+        };
+
+        function adicionar(veiculo: Veiculo, salva?: boolean){
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+                <button class="delete" data-placa="${veiculo.placa}">X</button>
+            </td>
+            `;
+            $('#patio')?.appendChild(row);
+
+            if(salva) salvar([...ler(), veiculo]);
+
+        };
+
+        function remover(){};
+
+        function render(){
+            $("#patio")!.innerHTML = "";
+            const patio = ler();
+
+            if(patio.length){
+                patio.forEach(veiculo => adicionar(veiculo));
+            }
+        };
+
+        return {ler, adicionar, remover, salvar, render};
+     }
+    
+     patio().render();
     $('#cadastrar')?.addEventListener("click", () => {
 
         var nome = $('#nome')?.value;
@@ -15,5 +63,7 @@
             alert('Campos Nome e Placa obrigatórios');
             return;
         }
+
+        patio().adicionar({nome, placa, entrada: new Date()}, true);
     })
 })();
