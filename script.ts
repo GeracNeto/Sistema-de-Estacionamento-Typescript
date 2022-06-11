cbea719fef8de278c889600bd4e1496b0f8c4c5b
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-10 21:32:05 -0300

    Criado função de remover,  convertido data para ISO, adicionado função para contar o tempo

--- script.ts
+++ script.ts
@@ -1,15 +1,20 @@
 // Interface
-
 interface Veiculo{
     nome: String;
     placa: String;
-    entrada: Date;
+    entrada: Date | string;
 }
-
 
 // Função anônima
 (function(){
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function calcTempo(mil: number){
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor(mil % 60000) / 1000;
+
+        return `${min}m e ${sec}s`;
+    }
 
      function patio(){
         function ler(): Veiculo[]{
@@ -30,13 +35,28 @@ interface Veiculo{
                 <button class="delete" data-placa="${veiculo.placa}">X</button>
             </td>
             `;
+
+            row.querySelector(".delete")?.addEventListener("click", function(){
+                remover(this.dataset.placa);
+            })
+
             $('#patio')?.appendChild(row);
 
             if(salva) salvar([...ler(), veiculo]);
 
         };
 
-        function remover(){};
+        function remover(placa: string){
+
+            const {entrada, nome} = ler().find(veiculo => veiculo.placa === placa);
+
+            const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
+
+            if(!confirm(`O veículo ${nome} permanceu por  ${tempo}. Deseja encerrar?`)) return;
+
+            salvar(ler().filter(veiculo => veiculo.placa !== placa));
+            render();
+        };
 
         function render(){
             $("#patio")!.innerHTML = "";
@@ -64,6 +84,6 @@ interface Veiculo{
             return;
         }
 
-        patio().adicionar({nome, placa, entrada: new Date()}, true);
+        patio().adicionar({nome, placa, entrada: new Date().toISOString()}, true);
     })
 })();
