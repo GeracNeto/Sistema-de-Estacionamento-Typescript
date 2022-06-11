9c366492624f5dc617b7a73454700abcc77a2288
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-10 21:32:24 -0300

    Compilado pelo script.ts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Interface
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -12,6 +11,11 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 (function () {
     var _a;
     var $ = function (query) { return document.querySelector(query); };
+    function calcTempo(mil) {
+        var min = Math.floor(mil / 60000);
+        var sec = Math.floor(mil % 60000) / 1000;
+        return "".concat(min, "m e ").concat(sec, "s");
+    }
     function patio() {
         function ler() {
             return localStorage.patio ? JSON.parse(localStorage.patio) : [];
@@ -22,15 +26,25 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
         }
         ;
         function adicionar(veiculo, salva) {
-            var _a;
+            var _a, _b;
             var row = document.createElement("tr");
             row.innerHTML = "\n            <td>".concat(veiculo.nome, "</td>\n            <td>").concat(veiculo.placa, "</td>\n            <td>").concat(veiculo.entrada, "</td>\n            <td>\n                <button class=\"delete\" data-placa=\"").concat(veiculo.placa, "\">X</button>\n            </td>\n            ");
-            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+            (_a = row.querySelector(".delete")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+                remover(this.dataset.placa);
+            });
+            (_b = $('#patio')) === null || _b === void 0 ? void 0 : _b.appendChild(row);
             if (salva)
                 salvar(__spreadArray(__spreadArray([], ler(), true), [veiculo], false));
         }
         ;
-        function remover() { }
+        function remover(placa) {
+            var _a = ler().find(function (veiculo) { return veiculo.placa === placa; }), entrada = _a.entrada, nome = _a.nome;
+            var tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
+            if (!confirm("O ve\u00EDculo ".concat(nome, " permanceu por  ").concat(tempo, ". Deseja encerrar?")))
+                return;
+            salvar(ler().filter(function (veiculo) { return veiculo.placa !== placa; }));
+            render();
+        }
         ;
         function render() {
             $("#patio").innerHTML = "";
@@ -53,6 +67,6 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
             alert('Campos Nome e Placa obrigatórios');
             return;
         }
-        patio().adicionar({ nome: nome, placa: placa, entrada: new Date() }, true);
+        patio().adicionar({ nome: nome, placa: placa, entrada: new Date().toISOString() }, true);
     });
 })();
